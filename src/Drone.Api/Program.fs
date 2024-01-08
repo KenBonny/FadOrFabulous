2add1e604127b3476346eff5864a12b22ab9a540
Ken Bonny <bonny.ken+github@gmail.com>
2024-01-08 17:35:43 +0100

    Add Swagger to Drone.API and update launch URL
    The Swashbuckle.AspNetCore package has been added for Swagger implementation in order to document the API. The launch URL has been updated from 'drones' to 'swagger'. Additionally, Swagger has been configured to provide a description for the Drone API and an option to try the API.

--- src/Drone.Api/Program.fs
+++ src/Drone.Api/Program.fs
@@ -2,11 +2,11 @@ module Program
 
 #nowarn "20"
 open Drone.Api.Database.DroneContext
-open Drone.Api.Features
 open Microsoft.AspNetCore.Builder
 open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.OpenApi.Models
 open Wolverine
 open Wolverine.Http
 
@@ -25,6 +25,12 @@ let main args =
     builder.Services
         .AddEndpointsApiExplorer()
         .AddDbContext<DroneContext>(configureEF)
+        .AddSwaggerGen(fun c ->
+            c.SwaggerDoc("v1", OpenApiInfo(
+                Version = "v1",
+                Title = "Drone API",
+                Description = "Drone API to track drones across the sky"
+            )))
     builder.Host.UseWolverine()
 
     let app = builder.Build()
@@ -32,6 +38,7 @@ let main args =
     app.UseHttpsRedirection()
     // app.UseAuthorization()
     app.MapWolverineEndpoints()
+    app.UseSwagger().UseSwaggerUI(fun o -> o.ConfigObject.TryItOutEnabled <- true)
 
     app.Run()
 
