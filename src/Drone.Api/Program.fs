b027e5e16462b55edc284206bf1534d2d6e045c2
Ken Bonny <bonny.ken+github@gmail.com>
2024-01-08 14:21:28 +0100

    Add initial project files and configure gitignore
    This commit adds essential files to the FadOrFab project for the Drone.Api section, including necessary project, solution and configuration files. It also adds a .gitignore file, both at the root level and in the .idea directory, to manage untracked files and directories, optimizing the repository for the Rider IDE.

---
+++ src/Drone.Api/Program.fs
@@ -0,0 +1,27 @@
+namespace Drone.Api
+#nowarn "20"
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+module Program =
+    [<Literal>]
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
+
+        exitCode
