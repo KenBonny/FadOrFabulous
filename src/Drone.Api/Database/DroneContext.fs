1a666e0b31177419920392d6241a0685639ca4ec
Ken Bonny <bonny.ken+github@gmail.com>
2024-01-08 16:13:50 +0100

    Add Entity Framework database context to Drone API
    A new file (DroneContext.fs) has been created in the database directory of the Drone API. This file represents an Entity Framework DbContext for the Drone data. The program has also been updated to use this new database context, with a connection to an in-memory database called 'DroneDb'.

---
+++ src/Drone.Api/Database/DroneContext.fs
@@ -0,0 +1,16 @@
+﻿module Drone.Api.Database.DroneContext
+
+open Drone.Api.Domain.Drone
+open Microsoft.EntityFrameworkCore
+
+type DroneContext =
+    inherit DbContext
+
+    new() = { inherit DbContext() }
+    new(options: DbContextOptions<DroneContext>) = { inherit DbContext(options) }
+
+    [<DefaultValue>]
+    val mutable private drones: DbSet<Drone>
+    member this.Drones
+        with get() = this.drones
+        and set value = this.drones <- value
--- src/Drone.Api/Program.fs
+++ src/Drone.Api/Program.fs
@@ -8,12 +8,17 @@ module Program =
     [<Literal>]
     let exitCode = 0
 
-    [<EntryPoint>]
-    let main args =
+let configureEF (options:DbContextOptionsBuilder) =
+    options.UseInMemoryDatabase("DroneDb")
+    ()
+
+[<EntryPoint>]
+let main args =
 
         let builder = WebApplication.CreateBuilder(args)
 
         builder.Services.AddControllers()
+        .AddDbContext<DroneContext>(configureEF)
 
         let app = builder.Build()
 
