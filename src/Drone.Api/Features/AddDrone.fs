000618b112ae839b488f71134914a1ffd2d26e4c
Ken Bonny <bonny.ken+github@gmail.com>
2024-01-09 10:39:09 +0100

    Add drone creation feature
    A new file "AddDrone.fs" has been added that contains the logic for creating a drone in the drone API project. This includes the functions and types to create a new drone and save it in the drone context. This addition expands the drone API's capabilities to handle drone creation requests.

---
+++ src/Drone.Api/Features/AddDrone.fs
@@ -0,0 +1,26 @@
+﻿module Drone.Api.Features.AddDrone
+
+open Drone.Api.Database.DroneContext
+open Drone.Api.Domain.Drone
+open Microsoft.AspNetCore.Http
+open Wolverine.Http
+
+type CreateDrone = {
+    Make: Make
+    Model: string
+}
+
+type CreatedDrone = DroneId
+
+[<WolverinePost("drone")>]
+let createDrone (droneDto: CreateDrone) (context: DroneContext) =
+    task {
+        let drone = {
+            Id = 0
+            Make = droneDto.Make
+            Model = Model droneDto.Model
+        }
+        context.Add drone |> ignore
+        let! _ = context.SaveChangesAsync()
+        return Results.Created("/drones", drone.Id)
+    }
