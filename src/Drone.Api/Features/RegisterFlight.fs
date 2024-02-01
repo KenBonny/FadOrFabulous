2e4f90398e4b11070a34b46f21013c97691deef7
Ken Bonny <bonny.ken+github@gmail.com>
2024-02-01 12:08:43 +0100

    Simplify collision detection

--- src/Drone.Api/Features/RegisterFlight.fs
+++ src/Drone.Api/Features/RegisterFlight.fs
@@ -8,44 +8,35 @@ open Microsoft.AspNetCore.Http
 open Microsoft.EntityFrameworkCore
 open Wolverine.Http
 
-type Collision =
-    | NoCollision
-    | Collision of Coordinate
-
 let collision first second =
     if first = second then
-        Collision first
+        Some first
     else
-        NoCollision
+        None
 
-let detectCollision' newPath existingPath =
-    let detectTakeOffOrLandingCollision coordinate =
-        match existingPath with
-        | TakeOff existingCoordinate -> collision coordinate existingCoordinate
-        | Waypoint _ -> NoCollision
-        | Land existingCoordinate -> collision coordinate existingCoordinate
-    let detectWaypointCollision coordinate =
-        match existingPath with
-        | TakeOff _ -> NoCollision
-        | Waypoint existingCoordinate -> collision coordinate existingCoordinate
-        | Land _ -> NoCollision
+let detectCollision newPath existingPath =
+    match newPath, existingPath with
+    | TakeOff coordinate, TakeOff existing -> collision coordinate existing
+    | TakeOff _, Waypoint _ -> None
+    | TakeOff coordinate, Land existing -> collision coordinate existing
+    | Waypoint _, TakeOff _ -> None
+    | Waypoint coordinate, Waypoint existing -> collision coordinate existing
+    | Waypoint _, Land _ -> None
+    | Land coordinate, TakeOff existing -> collision coordinate existing
+    | Land _, Waypoint _ -> None
+    | Land coordinate, Land existing -> collision coordinate existing
 
-    match newPath with
-    | TakeOff coordinate -> detectTakeOffOrLandingCollision coordinate
-    | Waypoint coordinate -> detectWaypointCollision coordinate
-    | Land coordinate -> detectTakeOffOrLandingCollision coordinate
-
-let rec detectCollision newFlightPath existingFlightPath =
+let rec detectCollisionAlongFlightPath newFlightPath existingFlightPath =
     match existingFlightPath with
-    | [] -> NoCollision
-    | existing :: rest ->
-        let possibleCollision =
-            newFlightPath
-            |> List.map (detectCollision' existing)
-            |> List.tryFind (function | Collision _ -> true | _ -> false)
-        match possibleCollision with
-        | Some collision -> collision
-        | None -> detectCollision rest newFlightPath
+    | [] -> None
+    | existing :: restOfFlight ->
+        newFlightPath
+        |> List.map (detectCollision existing)
+        |> List.choose id
+        |> List.sort
+        |> function
+            | [] -> detectCollisionAlongFlightPath restOfFlight newFlightPath
+            | first :: _ -> Some first
 
 let rec validateFlight (drone: Drone) existingFlights newFlightPath =
     if obj.ReferenceEquals(drone, null) then
@@ -54,8 +45,8 @@ let rec validateFlight (drone: Drone) existingFlights newFlightPath =
         let collision =
             existingFlights
             |> List.map _.Path
-            |> List.map (detectCollision newFlightPath)
-            |> List.choose (function | Collision coordinate -> Some coordinate | _ -> None)
+            |> List.map (detectCollisionAlongFlightPath newFlightPath)
+            |> List.choose id
             |> List.tryHead
         match collision with
         | Some coordinate -> FlightRejected($"Collision detected at {coordinate.Lat}::{coordinate.Long}")
@@ -72,10 +63,10 @@ let registerFlight (droneId: int) (trajectory: FlightPath list) (db: DroneContex
         let message = flightRegistered |> toMessage
         let result =
             match flightRegistered with
-                | FlightRejected reason -> struct (Results.NotFound(reason), message)
-                | FlightRegistered flight ->
-                    db.Flights.Add(flight) |> ignore
-                    struct (Results.Ok(flight.Id), message)
+            | FlightRejected reason -> struct (Results.NotFound(reason), message)
+            | FlightRegistered flight ->
+                db.Flights.Add(flight) |> ignore
+                struct (Results.Ok(flight.Id), message)
 
         let! _ = db.SaveChangesAsync()
         return result
